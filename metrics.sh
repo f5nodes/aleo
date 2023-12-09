2e5b8efe425063e50cd3926fcc54fccd778c77c5
Influenzo <52459025+vasylcode@users.noreply.github.com>
2023-12-09 14:27:41 -0700

    metrics.sh updated

--- metrics.sh
+++ metrics.sh
@@ -5,11 +5,16 @@ get_data() {
   curl -s "http://localhost:3033$endpoint" | jq -r .
 }
 
+get_latest_chain_height() {
+  curl -s "https://f5nodes.com/api/aleo/blocks" | jq -r .maxHeight
+}
+
 display_table() {
   echo "----------------------------------------------"
   echo "| Endpoint                        | Data      |"
   echo "----------------------------------------------"
-  echo "| Latest Height                   | $(get_data /testnet3/latest/height) |"
+  echo "| Latest Chain Height             | $(get_latest_chain_height) |"
+  echo "| Latest Node Height              | $(get_data /testnet3/latest/height) |"
   echo "| Latest Hash                     | $(get_data /testnet3/latest/hash) |"
   echo "| Latest State Root               | $(get_data /testnet3/latest/stateRoot) |"
   echo "| Peers Count                     | $(get_data /testnet3/peers/count) |"
